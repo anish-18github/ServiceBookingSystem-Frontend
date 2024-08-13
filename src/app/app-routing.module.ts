9f9320a78f1b225a5b6511e236677291d59f68a2
anish-18github <anishgaikar17@gmail.com>
2024-08-13 15:43:20 +0530

    modules and components got added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'company', loadChildren: () => import('./company/company.module').then(m => m.CompanyModule) }, { path: 'client', loadChildren: () => import('./client/client.module').then(m => m.ClientModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
