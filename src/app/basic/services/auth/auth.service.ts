b7275700d7a8349d960513cdc66c6db7fb44085e
anish-18github <anishgaikar17@gmail.com>
2024-08-16 13:09:15 +0530

    user storage is done

--- src/app/basic/services/auth/auth.service.ts
+++ src/app/basic/services/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { response } from 'express';
 import { map, Observable } from 'rxjs';
+import { UserStorageService } from '../storage/user-storage.service';
 
 const BASIC_URL = 'http://localhost:8080/';
 
@@ -12,7 +13,9 @@ export const AUTH_HEADER = 'authorization';
 })
 export class AuthService {
 
-  constructor(private http: HttpClient,) { }
+  constructor(private http: HttpClient,
+    private userStorageService: UserStorageService
+  ) { }
 
   registerClient(signupRequestDTO: any): Observable<any> {
     return this.http.post(BASIC_URL + "client/sign-up", signupRequestDTO);
@@ -27,9 +30,11 @@ export class AuthService {
       .pipe(
         map((res: HttpResponse<any>) => {
           console.log(res.body)
+          this. userStorageService.saveUser(res.body);
           const tokenLength = res.headers.get(AUTH_HEADER)?.length;
           const bearerToken = res.headers.get(AUTH_HEADER)?.substring(7, tokenLength);
           console.log(bearerToken);
+          this. userStorageService.saveToken(bearerToken);
           return res;
         })
       );
