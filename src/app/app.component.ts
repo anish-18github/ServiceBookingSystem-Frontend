beb6ef0ad75a3987f063075d6ab4d65c196c3de1
anish-18github <anishgaikar17@gmail.com>
2024-08-17 21:40:49 +0530

    differ login for different users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserStorageService } from './basic/services/storage/user-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Odd-Jobs-app';
+
+  isClientLoggedIn: boolean = UserStorageService.isClientLoggedIn();
+  isCompanyLoggedIn: boolean = UserStorageService.isCompanyLoggedIn();
+
+  constructor(private router: Router){}
+
+    ngOnInit(){
+      this.router.events.subscribe(event => {
+        this.isClientLoggedIn = UserStorageService.isClientLoggedIn();
+        this.isCompanyLoggedIn = UserStorageService.isCompanyLoggedIn();
+
+      })
+    }
+
+    logout(){
+      UserStorageService.signOut();
+      this.router.navigateByUrl('login');
+    }
 }
